6af33d9574ba754ed4f14c1ffa2c52e36ecaed32
Hugo Dijkstra <hugodijkstra@live.nl>
2018-10-01 15:22:24 +0200

    Added imageGetter and GamePanel

---
+++ Code/MemoryProjectFull/Class/GamePanel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MemoryProject
+{
+    public partial class Card : Control { public Image image; }
+    class GamePanel : Grid
+    {
+        public GamePanel()
+        {
+            Run();
+        }
+
+        public void Build(List<Card> cards, int xAmount, int yAmount)
+        {
+            //Cards.Count() needs to be more or equale too (x * y)
+            if (cards.Count() < xAmount * yAmount)
+            {
+                Console.WriteLine("Cards.Count() needs to be more or equale too (x * y)");
+                return;
+            }
+
+            Width = 1500 * xAmount;
+            Height = 300 * yAmount;
+
+            int k = 0;
+            for (int i = 0; i < xAmount; i++)
+            {
+                this.RowDefinitions.Add(new RowDefinition());
+                for (int j = 0; j < yAmount; j++)
+                {
+                    this.ColumnDefinitions.Add(new ColumnDefinition());
+                    k++;
+                    Card c = cards[k];
+                    Console.WriteLine(c.image.Source);
+                    Grid.SetColumn(c.image, j);
+                    Grid.SetRow(c.image, i);
+                    Children.Add(c.image);
+                }
+            }
+        }
+
+        public void Run()
+        {
+            List<Card> cards = new List<Card>();
+            List<BitmapImage> bitmapImages = ImageGetter.GetImagesByTheme("weed ", 30, 400);
+            for (int i = 0; i < bitmapImages.Count; i++)
+            {
+                Image im = new Image();
+                im.BeginInit();
+                im.Source = bitmapImages[i];
+                im.MaxHeight = 400;
+                im.Stretch = Stretch.Fill;
+                im.Width = 200;
+                im.EndInit();
+                Card card = new Card() { image = im };
+                cards.Add(card);
+            }
+            Build(cards, 8, 8);
+        }
+    }
+    partial class Card
+    {
+
+    }
+}
---
+++ Code/MemoryProjectFull/Class/ImageGetter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace MemoryProject
+{
+    class ImageGetter
+    {
+        public static List<BitmapImage> GetImagesByTheme(string theme, int amount, int wantedWidht)
+        {
+            List<BitmapImage> returnValue = new List<BitmapImage>();
+            try
+            {
+                Console.WriteLine(("https://www.google.com/search?q=" + theme + "&tbm=isch").Replace(' ', '+'));
+
+                HttpWebRequest webRequest = WebRequest.CreateHttp(("https://www.google.com/search?q=" + theme + "&tbm=isch&tbs=iar:t").Replace(' ', '+'));
+                webRequest.Accept = "text/html, application/xhtml+xml, */*";
+                webRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
+                WebResponse webResponse = webRequest.GetResponse();
+                Stream data = webResponse.GetResponseStream();
+
+                string html = "";
+                using (StreamReader sr = new StreamReader(data))
+                {
+                    html = sr.ReadToEnd();
+                }
+                string[] splits = { "class=\"rg_ic rg_i\"" };
+
+                string[] splitString = html.Split(splits, StringSplitOptions.None);
+
+                string lookFor = "src=";
+                int imageAmount = 0;
+                for (int i = 0; i < splitString.Length; i++)
+                {
+                    int readFrom = splitString[i].IndexOf(lookFor) + lookFor.Length + 1;
+                    string imageLocation = splitString[i].Substring(readFrom);
+                    imageLocation = imageLocation.Substring(0, imageLocation.IndexOf('"'));
+                    try
+                    {
+                        BitmapImage pb = GetImageFromWeb(imageLocation, wantedWidht);
+                        if (pb == null)
+                        {
+                            continue;
+                        }
+                        returnValue.Add(pb);
+                    }
+                    catch (System.Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+                File.WriteAllText("html.html", html);
+            }
+            catch(System.Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return returnValue;
+        }
+
+        private static BitmapImage GetImageFromWeb(string url, int decodeWidth)
+        {
+
+            BitmapImage bitmap = new BitmapImage();
+            try
+            {
+                bitmap = new BitmapImage(new Uri(url, UriKind.Absolute)) { DecodePixelWidth = decodeWidth };
+                return bitmap;
+            }
+            catch (System.Exception e)
+            {
+                Console.Write(e);
+                return null;
+            }
+        }
+
+        public static List<BitmapImage> GetImagesFromFolder(string path)
+        {
+            List<BitmapImage> returnValue = new List<BitmapImage>();
+
+            try
+            {
+                string[] filenames = Directory.GetFiles(path);
+                foreach (string filePath in filenames)
+                {
+                    try
+                    {
+                        BitmapImage image = new BitmapImage();
+                        returnValue.Add(image);
+                    }
+                    catch (System.Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+            catch (System.Exception e)
+            {
+                Console.Write(e.ToString());
+            }
+            return returnValue;
+        }
+    }
+}
