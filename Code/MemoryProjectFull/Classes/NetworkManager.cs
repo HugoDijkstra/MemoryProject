f026d2d54f29d3f8579f579a282e754a54c22ffd
Hugo Dijkstra <hugodijkstra@live.nl>
2018-10-01 15:02:08 +0200

    Change fil structure

---
+++ Code/MemoryProjectFull/Classes/NetworkManager.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+/*
+ * Command struckture
+ * 
+ * G = global
+ * P = private
+ * 
+ * AA/AB/AC/BB/enz = command
+ * 
+ * G/P:AA <- command key
+ * 
+ * bleh,blah,bloeh <- command data
+ * 
+ */
+
+public enum NetworkType {
+    None,
+    Host,
+    Client
+}
+
+public interface INetworkListener {
+    void OnRecieveCommand(string _command, string[] _data);
+}
+
+// G:NCON,0,bob
+
+public class NetworkCommand{
+
+    public string key;
+    public Action<string[]> callback;
+
+    public bool idCheck;
+
+    public NetworkCommand(string _key, Action<string[]> _callback, bool _idCheck = false, bool _autoActiate = false){
+        key = _key;
+        callback += _callback;
+        idCheck = _idCheck;
+
+        if (_autoActiate)
+            activate();
+    }
+    
+    public void send(string[] _data) {
+        NetworkManager.getInstance().send(key, _data);
+    }
+
+    public void send(string _data) {
+        NetworkManager.getInstance().send(key, new string[1] { _data });
+    }
+
+    public void activate() {
+        NetworkHandler.getInstance().addCommand(this);
+    }
+
+    public void disable() {
+        NetworkHandler.getInstance().removeCommmand(this);
+    }
+}
+
+public class NetworkHandler {
+    private static NetworkHandler _instance;
+
+    // connections with new clients
+    public Action OnLostConnectionToHost;
+
+    public Action<int> OnResyncRequest;
+
+    public Action<int> OnInitialized;
+    public Action<int> OnClientConnection;
+    public Action<int> OnClientDisconection;
+
+    private List<NetworkCommand> _serverCommands;
+    private List<NetworkCommand> _cleintCommands;
+
+    // vars for this client
+    public int networkID;
+
+    // server commands
+    NetworkCommand _serverQuit;
+    NetworkCommand _serverInit;
+    NetworkCommand _serverSyncConnection;
+    NetworkCommand _serverSyncRequest;
+    NetworkCommand _serverDisconectHost;
+    NetworkCommand _serverConnect;
+    NetworkCommand _serverDisconnect;
+
+    public NetworkHandler() {
+        _serverCommands = new List<NetworkCommand>();
+        _cleintCommands = new List<NetworkCommand>();
+
+        _serverQuit = new NetworkCommand("S:QUIT", (x) => {
+            Environment.Exit(1); // temp
+        });
+
+        _serverInit = new NetworkCommand("S:INIT", (x) => {
+            networkID = int.Parse(x[0]);
+            OnInitialized(networkID);
+            _serverConnect.send(networkID.ToString());
+        });
+
+        _serverDisconectHost = new NetworkCommand("S:NOHOST", (x) => {
+            OnLostConnectionToHost.Invoke();
+        });
+
+        _serverSyncConnection = new NetworkCommand("S:SYNC", (x) => { // call the request resync callback
+            OnResyncRequest?.Invoke(int.Parse(x[0]));
+        });
+
+        _serverSyncRequest = new NetworkCommand("S:RSYNC", (x) => { // send resync request to all clients but [id] (only host can do this)
+            //resyncClient(int.Parse(x[0]));
+
+            if (isHost()) {
+                ClientConnection[] clients = NetworkManager.getInstance().getClientConnections();
+                int length = clients.Length;
+                for (int i = 0; i < length; i++) {
+                    NetworkManager.getInstance().sendTargetCommand(clients[i].networkID, _serverSyncConnection, x);
+                }
+                OnResyncRequest?.Invoke(int.Parse(x[0]));
+            }
+        });
+
+        _serverConnect = new NetworkCommand("S:CONNECT", (x) => {
+            OnClientDisconection?.Invoke(int.Parse(x[0]));
+        });
+
+        _serverDisconnect = new NetworkCommand("S:DISCONNECT", (x) => {
+            OnClientDisconection?.Invoke(int.Parse(x[0]));
+        });
+
+        _serverCommands.Add(_serverQuit);
+        _serverCommands.Add(_serverInit);
+        _serverCommands.Add(_serverDisconectHost);
+        _serverCommands.Add(_serverSyncConnection);
+        _serverCommands.Add(_serverSyncRequest);
+        _serverCommands.Add(_serverConnect);
+        _serverCommands.Add(_serverDisconnect);
+    }
+
+    public void init(int _id) {
+        _serverInit.callback?.Invoke(new string[1] { _id.ToString() });
+    }
+
+    public static NetworkHandler getInstance() {
+        if (_instance == null) {
+            _instance = new NetworkHandler();
+        }
+        return _instance;
+    }
+    public void addCommand(NetworkCommand _command) {
+        if (!_cleintCommands.Contains(_command)) // <-- can be removed???
+            _cleintCommands.Add(_command);
+    }
+
+    public void removeCommmand(NetworkCommand _command) {
+        if (_cleintCommands.Contains(_command)) // <-- can be removed???
+            _cleintCommands.Remove(_command);
+    }
+
+    public bool isHost() {
+        return NetworkManager.getInstance().networkType == NetworkType.Host;
+    }
+
+    public void requestResync() {
+        _serverSyncRequest.send(new string[1] { networkID.ToString() });
+    }
+
+    public void onRecieveCommand(string _command, string[] _data){
+        for (int i = 0; i < _serverCommands.Count(); i++){
+            if (_serverCommands[i].key == _command) {
+                _serverCommands[i].callback?.Invoke(_data);
+                return;
+            }
+        }
+
+        for (int i = 0; i < _cleintCommands.Count(); i++){
+            if (_cleintCommands[i].key == _command) {
+                if(_cleintCommands[i].idCheck && networkID != int.Parse(_data[0]))
+                    _cleintCommands[i].callback?.Invoke(_data);
+                else if(!_cleintCommands[i].idCheck)
+                    _cleintCommands[i].callback?.Invoke(_data);
+            }
+        }
+    }
+
+}
+
+public class NetworkManager {
+    private static NetworkManager _instance;
+    private static int _uniqueID;
+
+    // callback to recieve a message from the host
+    public Action<string, string[]> OnRecieveCommand;
+
+    private TcpListener _host;
+
+    private Thread _checkConnectionThread;
+    private Thread _createConnectionThread;
+
+    private List<ClientConnection> _clientConnections;
+    private ClientConnection _clientConnection;
+
+    public NetworkType networkType;
+
+    private NetworkManager() {
+        _clientConnections = new List<ClientConnection>();
+        networkType = NetworkType.None;
+        _uniqueID = 0;
+    }
+
+    // get instance of network manager
+    public static NetworkManager getInstance() {
+        if (_instance == null) {
+            _instance = new NetworkManager();
+        }
+        return _instance;
+    }
+
+    public void create(NetworkType _type, string _ip, int _port, Action<int> _OnInitialized) {
+        create(_type, _ip, _port, _OnInitialized, 1024);
+    }
+
+    public void create(NetworkType _type, string _ip, int _port, Action<int> _OnInitialized, int _bufferSize) {
+        networkType = _type;
+
+        OnRecieveCommand += NetworkHandler.getInstance().onRecieveCommand;
+        NetworkHandler.getInstance().OnInitialized += _OnInitialized;
+
+        if (_type == NetworkType.None) {
+            return;
+        }
+        else if (_type == NetworkType.Host) {
+            // create host connection
+            _host = new TcpListener(new IPEndPoint(IPAddress.Any, _port));
+            _host.Start();
+
+            // activate connection listen thread
+            _checkConnectionThread = new Thread(new ThreadStart(onClientConnect));
+            _checkConnectionThread.Start();
+
+            // because this is host he init's on creation
+            NetworkHandler.getInstance().init(-1);
+        }
+        else if (_type == NetworkType.Client){
+            _createConnectionThread = new Thread(new ThreadStart(() => {
+                TcpClient tcpClient = new TcpClient();
+
+                while (true) {
+                    try{
+                        tcpClient.Connect(_ip, _port);
+                        break;
+                    }
+                    catch (Exception){
+                    }
+                }
+                
+                _clientConnection = new ClientConnection(tcpClient, _bufferSize);
+                _clientConnection.OnConnectionTerminate += terminateClient;
+                _clientConnection.OnReceiveData += recieveData;
+
+                _createConnectionThread.Abort();
+            }));
+            
+            NetworkHandler.getInstance().networkID = -2;
+            _createConnectionThread.Start();
+        }
+    }
+
+    public ClientConnection[] getClientConnections() {
+        return _clientConnections.ToArray();
+    }
+
+    public void sendTargetCommand(int _id, NetworkCommand _command, string[] _data) {
+        int length = _clientConnections.Count;
+        for (int i = 0; i < length; i++){
+            if (_clientConnections[i].networkID == _id) {
+                string message = formatCommand(_command.key, _data);
+                _clientConnections[i].send(message);
+                return;
+            }
+        }
+    }
+
+    private bool isHost() {
+        return networkType == NetworkType.Host;
+    }
+
+    // format 
+    private string formatCommand(string _id, string[] _data) {
+        string message = _id + ",";
+        for (int i = 0; i < _data.Length; i++){
+            message += _data[i];
+            if (i < _data.Length-1) {
+                message += ",";
+            }
+        }
+        return message;
+    }
+
+    // send message to all clients
+    public void send(string _id, string[] _data) {
+        // format data
+        string message = formatCommand(_id, _data);
+
+        string debug = "";
+        for (int i = 0; i < _data.Length; i++){
+            debug += _data[i] + ", ";
+        }
+        Console.WriteLine(string.Format("[{2}] SENDING -> {0} {1}", _id, debug, DateTime.UtcNow.Ticks.ToString()));
+
+        if (networkType == NetworkType.Host) {
+            // send data to all clients
+            for (int i = 0; i < _clientConnections.Count; i++){
+                _clientConnections[i].send(message);
+            }
+
+            // send data to self
+            recieveData(message);
+        }else if (networkType == NetworkType.Client){
+            _clientConnection.send(message);
+        }
+    }
+
+    // stop connection with all clients and close thread
+    public void terminate() {
+        if (_checkConnectionThread != null) {
+            _checkConnectionThread.Abort();
+            _checkConnectionThread = null;
+        }
+
+        for (int i = 0; i < _clientConnections.Count; i++){
+            ClientConnection c = _clientConnections[i];
+            _clientConnections.RemoveAt(i);
+            c.terminate();
+        }
+
+        if (_clientConnection != null) {
+            _clientConnection.terminate();
+            _clientConnection = null;
+        }
+    }
+
+    // recieve data from host
+    private void recieveData(string _message) {
+        string[] array = _message.Split(',');
+        string command = array[0];
+        string[] data = array.Skip(1).ToArray();
+
+        
+        string debug = "";
+        for (int i = 0; i < data.Length; i++){
+            debug += data[i] + ", ";
+        }
+        Console.WriteLine(string.Format("[{2}] RECIEVE -> {0} {1}", command, debug, DateTime.UtcNow.Ticks.ToString()));
+
+        OnRecieveCommand?.Invoke(command, data);
+    }
+
+    // on client connect
+    private void onClientConnect() {
+        while (true) { 
+            TcpClient client = _host.AcceptTcpClient();
+            Console.WriteLine("Connected to new client");
+            ClientConnection cConnection = new ClientConnection(client, 1024);
+             
+            cConnection.OnConnectionTerminate += terminateClient;
+            cConnection.OnReceiveData += (x) => {
+
+                string[] array = x.Split(',');
+                string command = array[0];
+                string[] data = array.Skip(1).ToArray();
+                send(command, data);
+
+            };
+
+            // send client id to client ( this init's the client )
+            int networkID = _uniqueID;
+            _uniqueID++;
+
+            cConnection.send("S:INIT," + networkID);
+            cConnection.networkID = networkID;
+            _clientConnections.Add(cConnection);
+        }
+    }
+
+    // on terminate client
+    private void terminateClient(ClientConnection c) {
+        if (isHost()) {
+            int id = c.networkID;
+            _clientConnections.Remove(c);
+            c.terminate();
+            send("S:DISCONNECT", new string[1] { id.ToString() });
+        }else {
+            recieveData("S:NOHOST");
+        }
+    }
+
+}
+
+public class ClientConnection{
+
+    public int networkID;
+
+    public Action<string> OnReceiveData;
+    public Action<ClientConnection> OnConnectionTerminate;
+
+    private TcpClient _tcpClient;
+    private NetworkStream _stream;
+    private byte[] _buffer;
+
+    private Thread _clientListener;
+
+    public ClientConnection(TcpClient _tcpC, int _bufferSize) {
+        _tcpClient = _tcpC;
+        _stream = _tcpClient.GetStream();
+
+        _buffer = new byte[_bufferSize];
+
+        _clientListener = new Thread(new ThreadStart(listen));
+        _clientListener.Start();
+    }
+
+    // send messages to the server
+    public void send(string _message) {
+        if(_tcpClient != null) { 
+            byte[] byteMessage = Encoding.ASCII.GetBytes(_message);
+            _stream.Write(byteMessage, 0, byteMessage.Length);
+        }
+    }
+    
+    // listen to the server and prosses the messages
+    private void listen() {
+        string message = "";
+        int length = 0;
+
+        while (true){
+            try{
+                length = _stream.Read(_buffer, 0, _buffer.Length);
+            }
+            catch (Exception){
+                break;
+            }
+
+            message = Encoding.ASCII.GetString(_buffer, 0, length);
+            OnReceiveData?.Invoke(message);
+        }
+
+        OnConnectionTerminate?.Invoke(this);
+    }
+
+    // stop the connection and abort the thread
+    public void terminate() {
+        if (_clientListener.ThreadState == ThreadState.Running){
+            _clientListener = null;
+        }
+
+        if (_tcpClient != null) {
+            _tcpClient.Close();
+        }
+
+        Console.WriteLine("Stopped connection with client");
+    }
+
+}
---
+++ Code/MemoryProjectFull/Classes/StatsManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Jil;
+
+using Database = System.Collections.Generic.Dictionary<string, MemoryProject.Stats>;
+
+namespace MemoryProject
+{
+
+    public class StatsManager
+    {
+
+        public StatsManager()
+        {
+            this.database = new Database();
+        }
+
+        public static StatsManager LoadFromJSON(string filePath)
+        {
+            StatsManager statsManager = new StatsManager();
+
+            if (File.Exists(filePath))
+            {
+                using (var fstream = File.OpenRead(filePath))
+                using (var input = new StreamReader(fstream))
+                {
+                    statsManager.database = JSON.Deserialize<Database>(input);
+                }
+            }
+            else
+            {
+                using (var fstream = File.Create(filePath))
+                using (var output = new StreamWriter(fstream))
+                {
+                    output.Write(JSON.Serialize<Database>(statsManager.database));
+                }
+            }
+
+            return statsManager;
+        }
+
+        public void WriteToJSON(string filePath)
+        {
+            File.WriteAllText(filePath, string.Empty); //Clear the file before writing.
+
+            using (var fstream = File.OpenWrite(filePath))
+            using (var output = new StreamWriter(fstream))
+            {
+                output.Write(JSON.Serialize<Database>(database));
+            }
+        }
+
+        public bool StoreStats(Stats stats)
+        {
+            if (PlayerExists(stats.Name)) return false;
+
+            database.Add(stats.Name, stats);
+
+            return true;
+        }
+
+        public bool PlayerExists(string name)
+        {
+            return database.ContainsKey(name);
+        }
+
+        public Stats RetrieveStats(string name)
+        {
+            if (!PlayerExists(name)) return null;
+
+            return database[name];
+        }
+
+        public bool RemoveStats(string name)
+        {
+            if (!PlayerExists(name)) return false;
+
+            database.Remove(name);
+            return true;
+        }
+
+        private Database database;
+
+    }
+}
