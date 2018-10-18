9487d7bafee14d2520156e8bbb99fb7b6a27ce47
Hugo Dijkstra <hugodijkstra@live.nl>
2018-10-18 13:59:17 +0200

    Added URL getter

--- Code/MemoryProjectFull/Class/ImageGetter.cs
+++ Code/MemoryProjectFull/Class/ImageGetter.cs
@@ -12,6 +12,52 @@ namespace MemoryProjectFull
 {
     class ImageGetter
     {
+        public static List<string> GetUrlsByTheme(string theme, int amount)
+        {
+            List<string> returnValue = new List<string>();
+            try
+            {
+                Console.WriteLine(("https://www.google.com/search?q=" + theme + "&tbm=isch").Replace(' ', '+'));
+
+                HttpWebRequest webRequest = WebRequest.CreateHttp(("https://www.google.com/search?q=" + theme + "&tbm=isch&tbs=iar:t").Replace(' ', '+'));
+                webRequest.Accept = "text/html, application/xhtml+xml, */*";
+                webRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
+                WebResponse webResponse = webRequest.GetResponse();
+                Stream data = webResponse.GetResponseStream();
+
+                string html = "";
+                using (StreamReader sr = new StreamReader(data))
+                {
+                    html = sr.ReadToEnd();
+                }
+                string[] splits = { "class=\"rg_ic rg_i\"" };
+
+                string[] splitString = html.Split(splits, StringSplitOptions.None);
+
+                string lookFor = "src=";
+                int imageAmount = 0;
+                for (int i = 0; i < splitString.Length; i++)
+                {
+                    int readFrom = splitString[i].IndexOf(lookFor) + lookFor.Length + 1;
+                    string imageLocation = splitString[i].Substring(readFrom);
+                    imageLocation = imageLocation.Substring(0, imageLocation.IndexOf('"'));
+                    try
+                    {
+                        returnValue.Add(imageLocation);
+                    }
+                    catch (System.Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+                File.WriteAllText("html.html", html);
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return returnValue;
+        }
         public static List<BitmapImage> GetImagesByTheme(string theme, int amount, int wantedWidht)
         {
             List<BitmapImage> returnValue = new List<BitmapImage>();
@@ -64,7 +110,7 @@ namespace MemoryProjectFull
             return returnValue;
         }
 
-        private static BitmapImage GetImageFromWeb(string url, int decodeWidth)
+        public static BitmapImage GetImageFromWeb(string url, int decodeWidth)
         {
             if (!url.Contains("http"))
                 return null;
