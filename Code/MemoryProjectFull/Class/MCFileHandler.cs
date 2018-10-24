344b9afe72c6eea6ce43599bae7afd1b14955a7c
shcalbers <34623185+shcalbers@users.noreply.github.com>
2018-10-24 17:32:32 +0200

    Added MemoryConfig.

---
+++ Code/MemoryProjectFull/Class/MCFileHandler.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryProjectFull
+{
+
+    public partial class MemoryConfig
+    {
+
+        private static class FileHandler
+        {
+
+            private const long MAGIC_NUMBER = 0x54554E475277;
+            private const int END_OF_FILE = -1;
+
+            private enum Token : byte { GROUP_NAME = 1, ENTRY_NAME = 2, ENTRY_VALUE = 4 }
+            private enum Types : byte { INT32, STRING }
+
+            private static bool MagicNumberIsValid(long magicNumber)
+            {
+                return (magicNumber == MAGIC_NUMBER);
+            }
+
+            public static void Compose(MemoryConfig config, string filePath)
+            {
+                File.WriteAllText(filePath, string.Empty);
+
+                using (var file = File.OpenWrite(filePath))
+                using (var writer = new BinaryWriter(file))
+                {
+                    writer.Write(MAGIC_NUMBER);
+
+                    foreach (var group in config.Groups())
+                    {
+                        writer.Write((byte)Token.GROUP_NAME);
+                        writer.Write(group.GetName());
+
+                        foreach (var entry in group.Entries())
+                        {
+                            writer.Write((byte)Token.ENTRY_NAME);
+                            writer.Write(entry.GetName());
+                            writer.Write((byte)Token.ENTRY_VALUE);
+
+                            if (entry.GetValueType() == typeof(int))
+                            {
+                                writer.Write((byte)Types.INT32);
+                                writer.Write(entry.GetValue<int>());
+                            }
+                            else if (entry.GetValueType() == typeof(string))
+                            {
+                                writer.Write((byte)Types.STRING);
+                                writer.Write(entry.GetValue<string>());
+                            }
+                            else
+                            {
+                                throw new IOException("Failed to write config! Unknown Type: " + entry.GetValueType());
+                            }
+                        }
+                    }
+
+                    writer.Flush();
+                }
+            }
+
+            public static void Parse(MemoryConfig config, string filePath)
+            {
+                DataGroup currentGroup  = null; 
+                string currentEntryName = null;
+
+                using (var file = File.OpenRead(filePath))
+                using (var reader = new BinaryReader(file))
+                {
+                    if (!MagicNumberIsValid( reader.ReadInt64() )) throw new IOException("'" + filePath + "' is not a valid .MCFG file.");
+
+                    var expectedTokens = (byte) Token.GROUP_NAME;
+
+                    while (reader.PeekChar() != END_OF_FILE)
+                    {
+                        var token = reader.ReadByte();
+                        if ((expectedTokens & token) == 0) throw new IOException("Failed to read '" + filePath + "'! Invalid Token!");
+
+                        switch ((Token) token)
+                        {
+                            case Token.GROUP_NAME:
+                                string groupName = reader.ReadString();
+
+                                config.AddGroup(groupName);
+                                currentGroup = config.GetGroup(groupName);
+
+                                expectedTokens = (byte)Token.ENTRY_NAME;
+                                break;
+                            case Token.ENTRY_NAME:
+                                currentEntryName = reader.ReadString();
+
+                                expectedTokens = (byte)Token.ENTRY_VALUE;
+                                break;
+                            case Token.ENTRY_VALUE:
+                                byte type = reader.ReadByte();
+
+                                switch (type)
+                                {
+                                    case (byte) Types.INT32:
+
+                                        currentGroup.AddEntry(currentEntryName, reader.ReadInt32());
+                                        break;
+
+                                    case (byte) Types.STRING:
+
+                                        currentGroup.AddEntry(currentEntryName, reader.ReadString());
+                                        break;
+
+                                    default:
+
+                                        throw new IOException("Failed to read '" + filePath + "'! Unknown Value Type!" + type);
+                                }
+
+                                expectedTokens = (byte)Token.ENTRY_NAME | (byte)Token.GROUP_NAME;
+                                break;
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Code/MemoryProjectFull/Class/MemoryConfig.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryProjectFull
+{
+    
+    public class DataEntry
+    {
+
+        public DataEntry(string name, int value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public DataEntry(string name, string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public Type GetValueType()
+        {
+            return value.GetType();
+        }
+
+        public ValueType GetValue<ValueType>()
+        {
+            return (ValueType) value;
+        }
+
+        public void SetValue(int newValue)
+        {
+            value = newValue;
+        }
+
+        public void SetValue(string newValue)
+        {
+            value = newValue;
+        }
+
+        private readonly string name;
+        private object value;
+
+    }
+
+    public class DataGroup
+    {
+
+        public DataGroup(string name)
+        {
+            this.name = name;
+            this.entries = new Dictionary<string, DataEntry>();
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public bool HasEntry(string name)
+        {
+            return entries.ContainsKey(name);
+        }
+
+        public DataGroup AddEntry(string name, int value)
+        {
+            try
+            {
+                entries.Add(name, new DataEntry(name, value));
+                return this;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public DataGroup AddEntry(string name, string value)
+        {
+            try
+            {
+                entries.Add(name, new DataEntry(name, value));
+                return this;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public DataEntry GetEntry(string name)
+        {
+            try
+            { return entries[name]; }
+            catch
+            { return null; }
+        }
+
+        public bool RemoveEntry(string name)
+        {
+            return entries.Remove(name);
+        }
+
+        public IEnumerable<DataEntry> Entries()
+        {
+            foreach (var entry in entries)
+            {
+                yield return entry.Value;
+            }
+        }
+
+        private readonly string name;
+        private Dictionary<string, DataEntry> entries;
+
+    }
+
+    public partial class MemoryConfig
+    {
+
+        public MemoryConfig()
+        {
+            this.groups = new Dictionary<string, DataGroup>();
+        }
+
+        public static MemoryConfig LoadFromFile(string filePath)
+        {
+            MemoryConfig config = new MemoryConfig();
+            FileHandler.Parse(config, filePath);
+
+            return config;
+        }
+
+        public void SaveToFile(string filePath)
+        {
+            FileHandler.Compose(this, filePath);
+        }
+
+        public bool HasGroup(string name)
+        {
+            return groups.ContainsKey(name);
+        }
+
+        public MemoryConfig AddGroup(string name)
+        {
+            try
+            {
+                groups.Add(name, new DataGroup(name));
+                return this;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public DataGroup GetGroup(string name)
+        {
+            try
+            { return groups[name]; }
+            catch
+            { return null; }
+        }
+
+        public bool RemoveGroup(string name)
+        {
+            return groups.Remove(name);
+        }
+
+        public IEnumerable<DataGroup> Groups()
+        {
+            foreach (var group in groups)
+            {
+                yield return group.Value;
+            }
+        }
+
+        private Dictionary<string, DataGroup> groups;
+
+    }
+
+}
